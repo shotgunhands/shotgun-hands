66f0139c2357be46b20deb8d6b8c33cce3a640da
Passkii <neopetersson5@gmail.com>
2024-05-01 17:11:54 +0200

    SSStyle guidelines followed

--- UIs/Console/console.gd
+++ UIs/Console/console.gd
@@ -1,14 +1,14 @@
 extends CanvasLayer
 
 ### VARIABLES ###
-@onready var __input:LineEdit = %LineEdit
-@onready var _output:RichTextLabel = %RichTextLabel
+@onready var _input_line: LineEdit = %LineEdit
+@onready var _output_label: RichTextLabel = %RichTextLabel
 
 var _history: Array[String] = [""]
 
 ### METHODS ###
 func _ready() -> void:
-	__input.gui_input.connect(_on_input)
+	_input_line.gui_input.connect(_on_input)
 	clear()
 	visible = false
 
@@ -16,36 +16,36 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("_dev_console_toggle"):
 		visible = !visible
 
-func _on_input(event:InputEvent) -> void:
+func _on_input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.keycode == KEY_UP and event.pressed:
-			__input.text = _history[0]
+			_input_line.text = _history[0]
 		if event.keycode == KEY_ENTER and event.pressed:
 			if event is InputEventWithModifiers and not event.shift_pressed:
-				var expression:Expression = Expression.new()
+				var expression: Expression = Expression.new()
 
-				_history.push_front(__input.text)
+				_history.push_front(_input_line.text)
 
-				var parse_error = expression.parse(__input.text)
+				var parse_error = expression.parse(_input_line.text)
 				if parse_error != OK:
-					cast_message("[color=red]"+expression.get_error_text()+"[/color]")
+					cast_message("[color=red]" + expression.get_error_text() + "[/color]")
 
 				var execute_result = expression.execute([], self)
 				if expression.has_execute_failed():
-					cast_message("[color=red]"+expression.get_error_text()+"[/color]")
+					cast_message("[color=red]" + expression.get_error_text() + "[/color]")
 				elif execute_result != null:
 					if not execute_result is Object:
-						_output.text += String(execute_result) + '\n'
+						_output_label.text += String(execute_result) + '\n'
 
-
-				__input.text = ""
+				_input_line.text = ""
 
 ### COMMANDS ###
-func cast_message(msg:String) -> void: _output.text += msg + '\n'
+func cast_message(msg: String) -> void:
+	_output_label.text += msg + '\n'
 
-func clear()->void: _output.text = ""
+func clear() -> void:
+	_output_label.text = ""
 
-func get_history()->void:
+func get_history() -> void:
 	for i in _history:
 		cast_message(i)
-
