5cdab14cf5f9409f0c654e0868b8a354b92b0270
Mikey9124x <mikey9124x@gmail.com>
2024-05-02 17:32:45 -0500

    Made antimony

---
+++ Actors/Enemies/Antimony/ProjectileBase.gd
@@ -0,0 +1,21 @@
+class_name BaseProjectile
+extends HitableCharacterBody
+	
+var player:Node2D          = null
+var SPEED = 300
+func _ready():
+	player = get_tree().get_nodes_in_group("Player")[0]
+	var dir_to_player = (player.global_position - global_position).normalized()
+	velocity.x = SPEED * dir_to_player.x
+	velocity.y = SPEED * dir_to_player.y
+func _physics_process(_delta):
+	var distance_to_player = player.global_position.distance_to(global_position)
+
+
+	move_and_slide()
+	if (distance_to_player <= 40):
+		mel_attack()
+func mel_attack():
+	
+	player.health -= 10
+	destroy()
---
+++ Actors/Enemies/Antimony/antimony_potion.gd
@@ -0,0 +1,7 @@
+class_name Antimony_Potion
+extends HitableCharacterBody
+
+### CONSTANTS ###
+@export var SPEED = 100
+### ENUMS ###
+func(rea
