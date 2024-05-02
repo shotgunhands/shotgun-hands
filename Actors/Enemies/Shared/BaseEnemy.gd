114dc0e5770d6e75af21de84f31a86a3ced0937f
Passkii <neopetersson5@gmail.com>
2024-05-02 18:46:27 +0200

    Finish placeholder code, the code is meant to be overriden anyways lol

--- Actors/Enemies/Shared/BaseEnemy.gd
+++ Actors/Enemies/Shared/BaseEnemy.gd
@@ -12,24 +12,27 @@ var has_seen: bool = false;
 var direction = 1
 var player = null
 
+### REFERENCES ###
+@onready var anim_player:AnimationPlayer = $AnimationPlayer
 
 ### METHODS ###
 func _ready():
 	super()
-	player = get_node("../2DPlayer") # TODO: there is probably a better way to do this
+	player = get_tree().get_nodes_in_group("Player")[0]
 
 func _physics_process(delta) -> void:
 	if kill_check(): return
 
-	velocity.y += GRAVITY
+	if !is_on_floor(): velocity.y += GRAVITY
 
 	move_logic(delta)
 	attack_logic()
 	move_and_slide()
 
 func move_logic(_delta:float) -> void:
-	if is_on_wall():
-		direction *= -1
+	if anim_player.is_playing(): return
+
+	if is_on_wall(): direction *= -1
 
 	if check_can_see(): has_seen = true
 
@@ -40,10 +43,12 @@ func move_logic(_delta:float) -> void:
 		velocity.x = SPEED * direction
 
 func attack_logic() -> void:
+	if anim_player.current_animation == "ATK" && anim_player.is_playing(): return
+
 	var distance_to_player = player.global_position.distance_to(global_position)
 	if distance_to_player <= MEL_ATTACK_RANGE:
+		anim_player.play("ATK")
 		mel_attack()
-
 
 func check_can_see() -> bool:
 	var space_state:PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
@@ -59,7 +64,6 @@ func check_can_see() -> bool:
 	var result = space_state.intersect_ray(query)
 
 	return result["collider"].is_in_group("Player") if !result.is_empty() && result["collider"] != null else false
-
 
 func mel_attack() -> void:
 	player.health -= 10
--- Globals/gamemanager.gd
+++ Globals/gamemanager.gd
@@ -3,5 +3,5 @@ extends Node
 func _ready():
 	pass
 
-func _process(delta):
+func _process(_delta):
 	pass
