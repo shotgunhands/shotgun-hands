94b99a50a164080aefc8c92b0ccae1179e86adc2
Passkii <neopetersson5@gmail.com>
2024-05-02 19:13:23 +0200

    better ai wowowowowowowowow

--- Actors/Enemies/Shared/BaseEnemy.gd
+++ Actors/Enemies/Shared/BaseEnemy.gd
@@ -6,57 +6,72 @@ extends HitableCharacterBody
 @export var SPEED = 200
 @export var GRAVITY = 20
 @export var JUMP_SPEED = -500
+@export var ATTACK_COOLDOWN = 1.0
+
+### ENUMS ###
+enum State {
+	IDLE,
+	CHASE,
+	ATTACK
+}
 
 ### VARIABLES ###
-var has_seen: bool = false;
-var direction = 1
-var player = null
-
-### REFERENCES ###
-@onready var anim_player:AnimationPlayer = $AnimationPlayer
+var has_seen: bool         = false
+var direction: float       = 1
+var player:Node2D          = null
+var state:State            = State.IDLE
+var last_attack_time:float = 0.0
 
 ### METHODS ###
 func _ready():
 	super()
 	player = get_tree().get_nodes_in_group("Player")[0]
 
-func _physics_process(delta) -> void:
+func _physics_process(_delta) -> void:
 	if kill_check(): return
 
 	if !is_on_floor(): velocity.y += GRAVITY
 
-	move_logic(delta)
-	attack_logic()
+	state_logic()
 	move_and_slide()
 
-func move_logic(_delta:float) -> void:
-	if anim_player.is_playing(): return
+func state_logic() -> void:
+	var can_see_player = check_can_see()
+	var distance_to_player = player.global_position.distance_to(global_position)
 
-	if is_on_wall(): direction *= -1
-
-	if check_can_see(): has_seen = true
-
-	if has_seen:
-		var dir_to_player = (player.global_position - global_position).normalized()
-		velocity.x = SPEED * dir_to_player.x
+	if can_see_player and distance_to_player <= MEL_ATTACK_RANGE:
+		state = State.ATTACK
+	elif can_see_player:
+		state = State.CHASE
 	else:
-		velocity.x = SPEED * direction
+		state = State.IDLE
+
+	match state:
+		State.IDLE:
+			idle_logic()
+		State.CHASE:
+			chase_logic()
+		State.ATTACK:
+			attack_logic()
+
+func idle_logic() -> void:
+	velocity.x = 0
+
+func chase_logic() -> void:
+	var dir_to_player = (player.global_position - global_position).normalized()
+	velocity.x = SPEED * dir_to_player.x
 
 func attack_logic() -> void:
-	if anim_player.current_animation == "ATK" && anim_player.is_playing(): return
-
-	var distance_to_player = player.global_position.distance_to(global_position)
-	if distance_to_player <= MEL_ATTACK_RANGE:
-		anim_player.play("ATK")
+	var current_time = Time.get_ticks_msec() / 1000.0
+	if current_time - last_attack_time > ATTACK_COOLDOWN:
 		mel_attack()
+		last_attack_time = current_time
 
 func check_can_see() -> bool:
 	var space_state:PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
 
 	var from: Vector2 = global_transform.origin
 	var to: Vector2 = player.global_transform.origin
-
-	from.y -= 45
 
 	var query = PhysicsRayQueryParameters2D.create(from, to)
 	query.exclude = [self]
