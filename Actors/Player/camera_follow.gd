25e8b15c9b845700dcdafbeaf57a7745522cc1a6
wdbros <wdbros@gmail.com>
2024-05-04 00:26:18 +0300

    post-merge unfucking PRIORITY NUMBER 1 is to do a refactor of the gun and enemy code, so that they're compatible and not fucky some files have to be sorted into proper places, and some code will have to be trimmed KEEP THINGS SIMPLE, sprites and other bells and whistles to a minimum until we have decent behaviors in place, please

--- Actors/Player/camera_follow.gd
+++ Actors/Player/camera_follow.gd
@@ -5,4 +5,4 @@ extends Camera2D
 @onready var player = $"../2DPlayer"
 
 func _process(_delta) -> void:
-	global_position.x = player.global_position.x+(player.velocity.x*velCChangeMul)
+	global_position.x = player.global_position.x + (player.velocity.x * velCChangeMul)
