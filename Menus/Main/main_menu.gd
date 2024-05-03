4b2993136a21b9012336869fab53358a0ad7fd10
Passkii <neopetersson5@gmail.com>
2024-05-03 16:21:45 +0200

    Main menu organisation

---
+++ Menus/Main/main_menu.gd
@@ -0,0 +1,17 @@
+class_name MainMenu extends Node
+
+@onready var play_btn: Button = $Center/VBox/Play
+@onready var quit_btn: Button = $Center/VBox/Quit
+
+func _ready() -> void:
+	_connect_signals()
+
+func _on_play():
+	Scenemanager.change_scene("playground")
+
+func _on_quit():
+	get_tree().quit()
+
+func _connect_signals():
+	play_btn.pressed.connect(_on_play)
+	quit_btn.pressed.connect(_on_quit)
