c3723ad3d60d0945fee2ec547497c5c10c8f800c
Mikey9124x <mikey9124x@gmail.com>
2024-05-03 19:07:27 -0500

    Glith

--- Actors/Enemies/Antimony/ProjectileBase.gd
+++ Actors/Enemies/Antimony/ProjectileBase.gd
@@ -13,7 +13,7 @@ func _ready():
 func _physics_process(_delta):
 	var distance_to_player = player.global_position.distance_to(global_position)
 	move_and_slide()
-	if (distance_to_player <= 60):
+	if (distance_to_player <= 30):
 		mel_attack()
 func mel_attack():
 	
