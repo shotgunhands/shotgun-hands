68315de7f979969e4f5e830f690bc509af688cc9
Mikey9124x <mikey9124x@gmail.com>
2024-06-16 15:56:36 -0500

    Update 2d_player.gd

--- Actors/Player/Scripts/2d_player.gd
+++ Actors/Player/Scripts/2d_player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
-
+var health = 100
 @onready var movement_controller: MovementController = $MovementController
 @onready var firing_controller: FiringController = $FiringController
-
+func _physics_process(delta):
+	if(health <=0):
+		get_tree().change_scene("res://Menus/Main/MainMenu.tscn")
 func lose_control():
 	movement_controller.lose_control()
