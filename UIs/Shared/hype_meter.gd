ab7d76f5a58cd58fd8c727cfb7a994215070ddf2
Ansterar <Abellingesq@gmail.com>
2024-06-19 14:29:31 +1000

    Added a way to increase hype
     - Added a placeholder button that will increase the hype meter when pressed

--- UIs/Shared/hype_meter.gd
+++ UIs/Shared/hype_meter.gd
@@ -2,6 +2,7 @@ extends Control
 
 var rank = "P"
 var hype = 100
+var delay_seconds = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,19 +11,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	match(rank):
-		"C":
-			pass
-		"B":
-			hype -= (100/6)*delta
-		"A":
-			hype -= (100/5)*delta
-		"S":
-			hype -= (100/4)*delta
-		"P":
-			hype -= (100/2)*delta
-		_:
-			pass
+	if delay_seconds <= 0:
+		match(rank):
+			"C":
+				pass
+			"B":
+				hype -= (100/6)*delta
+			"A":
+				hype -= (100/5)*delta
+			"S":
+				hype -= (100/4)*delta
+			"P":
+				hype -= (100/2)*delta
+			_:
+				pass
+	else:
+		delay_seconds -= delta
 	if hype <= 0:
 		hype = 100
 		match(rank):
@@ -46,9 +50,10 @@ func _process(delta):
 	$HypeProg.value = hype
 
 func increase_hype(amount):
+	delay_seconds = 0.5
 	hype += amount
-	if hype >= 100:
-		hype = 0
+	while hype >= 100:
+		hype -= 100
 		match(rank):
 			"C":
 				rank = "B"
@@ -60,6 +65,12 @@ func increase_hype(amount):
 				rank = "P"
 			"P":
 				hype = 100
+				break
 			_:
 				rank = "C"
 				hype = 0
+
+
+# Note: This is just a placeholder for now. Once hype increasing has properly been implemented, this can be removed
+func _on_increase_hype_placeholder_pressed():
+	increase_hype(20)
