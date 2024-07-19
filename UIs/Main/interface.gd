537b885cf8a594e2316d23eaaf7bcc87bb641273
Ansterar <Abellingesq@gmail.com>
2024-07-20 09:37:31 +1000

    Re-implemented Hype meter, and UI from original branch that they were on
    From "Implemented Hype Meter again" commit:
     - Added hype meter as described in the design document
     - Included a temporary button to increase the hype
     - Fixed the control_labels UI so that is always in the same place on screen
    
    from "Added a proper UI mockup" commit:
     - created main_ui.tscn scene
     - added placeholder labels for ammo, hit points, overheat and hype boost
     - added the hype meter to this UI
     - reverted previous changes to the control_labels.tscn to avoid interference (see previous commit)
    (The UI health no longer connects to the player's health since it doesn't appear to be implemented in this version)

---
+++ UIs/Main/interface.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@export var player_hold : CharacterBody2D
+@onready var health_bar : ProgressBar = $CanvasLayer/VBoxContainer/PlayerHealth
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+#	health_bar.value = player_hold.health
