7c982cad7d47d31b973573c4387fe264e3737a68
Ansterar <Abellingesq@gmail.com>
2024-07-20 11:00:20 +1000

    Reworked overheat times
     - Fixed slugshot having a maximum ammo of three
     - Changed time taken for overheat to drain when outside of overheat and in overheat
     - Changed bullet shooting time so that the player can actually build up heat, and also shoot faster

--- Actors/Player/Scripts/firing_controller.gd
+++ Actors/Player/Scripts/firing_controller.gd
@@ -28,6 +28,8 @@ var _touched_ground = 0
 
 @export var _overheat_melee_damage: float
 var _overheat: int = 0
+var _heat_cooldown_time : float = 1.7
+var _heat_active_cooldown : float = 1
 var _is_overheated: bool = false
 const OVERHEAT_THRESHOLD: int = 8
 @export var _overheat_timer: Timer
@@ -148,12 +150,14 @@ func _overheat_timeout():
 
 func _start_overheat():
 	_is_overheated = true
+	_overheat_timer.wait_time = _heat_active_cooldown
 	_melee_cooldown_timer.wait_time = _overheated_melee_cooldown_duration
 	_placeholder_visual_box.color = _overheated_color
 
 
 func _end_overheat():
 	_is_overheated = false
+	_overheat_timer.wait_time = _heat_cooldown_time
 	_melee_cooldown_timer.wait_time = _melee_cooldown_duration
 	_placeholder_visual_box.color = _default_color
 
