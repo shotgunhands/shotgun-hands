af1ffcdbb315fce716fdaa63ab2538762faef8d5
PersonCyclical <172178043+PersonCyclical@users.noreply.github.com>
2024-09-14 21:19:42 +1000

    Movement tweaks (#18)
    Movement changes:
    - made the jump much less "floaty"
    - added camera follow
    - began to work on coyote time and jump buffer time (currently disabled)
    - Fixed issue with shotgun jumping not working correctly on some devices
    - refactored animation to a separate function
    - Added more obstacles to the sandbox/dev level
    
    ---------
    
    Co-authored-by: Passkii <neopetersson5@gmail.com>

--- Actors/Enemies/TestEnemy/melee_test_enemy.gd
+++ Actors/Enemies/TestEnemy/melee_test_enemy.gd
@@ -3,5 +3,7 @@ class_name MeleeTestEnemy
 
 func _idle_logic() -> void:
 	velocity.x = 50
-	
 	move_and_slide()
+
+func _on_destroy() -> void:
+	queue_free()
--- Actors/Player/Scripts/camera_follow.gd
+++ Actors/Player/Scripts/camera_follow.gd
@@ -1,8 +1,19 @@
-extends Camera2D
+class_name Camera extends Camera2D
 
-@export var velCChangeMul: float = 0.258
+@export var SmoothCamera : bool = true
+@export var SmoothSpeed : float = 10.0 # Speed of camera smoothing
+@export var MouseInfluence : float = 0.2 # How much the mouse position influences the camera
 
 @onready var player = $"../2DPlayer"
 
+var camera_look_off: float
+
 func _process(_delta) -> void:
-	global_position.x = player.global_position.x + (player.velocity.x * velCChangeMul)
+	if player == null:
+		return
+
+	var mouse_pos = get_viewport().get_mouse_position()
+	var viewport_size = get_viewport_rect().size
+	var mouse_offset = (mouse_pos - viewport_size / 2) * MouseInfluence
+
+	position = player.position + mouse_offset
