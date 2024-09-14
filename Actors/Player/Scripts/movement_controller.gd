4205e1d96598c840b839e750e23092c9a55c9670
personCyclical <172178043+PersonCyclical@users.noreply.github.com>
2024-09-14 21:52:01 +1000

    Added another enemy
    Attempted merge

--- Actors/Player/Scripts/movement_controller.gd
+++ Actors/Player/Scripts/movement_controller.gd
@@ -89,8 +89,9 @@ func _physics_process(delta):
 	elif player.is_on_floor():
 		jumping = false
 
-	if !_jump_buffer_timer.is_stopped() and (player.is_on_floor() or (!_coyote_timer.is_stopped() and not jumping)):
-		jumping = true
+	# Handle jump buffering.
+	if jump_buffer_time_left > 0: jump_buffer_time_left -= delta
+	if jump_buffer_time_left > 0 and (player.is_on_floor() or coyote_time_left > 0):
 		player.velocity.y = jump_vel
 		_jump_buffer_timer.stop()
 
