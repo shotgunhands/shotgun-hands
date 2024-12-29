7d4766bf678f910234870674739e49a8b2450c71
UsUsStudios <biruni056@gmail.com>
2024-12-29 12:01:40 -0700

    Lowered bullet gravity and added bullet innacuracy

--- Ammunition/Slug/slug.gd
+++ Ammunition/Slug/slug.gd
@@ -5,6 +5,6 @@ func fire(pivot: Node2D):
 	super.fire(pivot)
 
 
-func _send_visual_pellet(angle: float, start_pos: Vector2, stop_pos: Vector2, ammo_base: AmmoType):
-	super._send_visual_pellet(angle, start_pos,stop_pos,ammo_base)
+func _send_visual_pellet(angle: float, start_pos: Vector2, stop_pos: Vector2, ammo_base: AmmoType, innacuracy: float):
+	super._send_visual_pellet(angle, start_pos,stop_pos,ammo_base, innacuracy)
 
