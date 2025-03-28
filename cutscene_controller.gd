d5929ce5e210d023f9e0974539df70b2f0d9426b
Ansterar <Abellingesq@gmail.com>
2025-03-29 09:26:13 +1100

    Cutscene camera functionality
    Added a new cutscene camera which normally follows the camera outside of cutscenes, but will become enabled and do whatever is needed during cutscenes

--- cutscene_controller.gd
+++ cutscene_controller.gd
@@ -1,5 +1,7 @@
 class_name CutscenePlayer extends AnimationPlayer
 
+@export
+var externalcam : Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,14 +9,18 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if not is_playing():
+		$CutsceneCam.position = externalcam.position
 
 
 func start_cut_scene(cutscenename) -> void:
 	get_tree().paused = true
 	play(cutscenename)
+	$CutsceneCam.enabled = true
+	$CutsceneCam.make_current()
 
 
-func _on_animation_finished(anim_name: StringName) -> void:
+func _on_animation_finished(_anim_name: StringName) -> void:
 	get_tree().paused = false
+	$CutsceneCam.enabled = false
