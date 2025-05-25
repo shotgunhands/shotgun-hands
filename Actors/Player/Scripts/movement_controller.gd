cd5980caa3ae50e76f0645590c6def204c631f5d
PersonCyclical <172178043+PersonCyclical@users.noreply.github.com>
2025-05-25 10:29:16 +1000

    (3 of 2) revert movement_controller.gd

--- Actors/Player/Scripts/movement_controller.gd
+++ Actors/Player/Scripts/movement_controller.gd
@@ -92,8 +92,8 @@ func _physics_process(delta):
 		jumping = false
 
 	# Handle jump buffering.
-	if jump_buffer_time_left > 0: jump_buffer_time_left -= delta
-	if jump_buffer_time_left > 0 and (player.is_on_floor() or coyote_time_left > 0):
+	if !_jump_buffer_timer.is_stopped() and (player.is_on_floor() or (!_coyote_timer.is_stopped() and not jumping)):
+		jumping = true
 		player.velocity.y = jump_vel
 		_jump_buffer_timer.stop()
 
